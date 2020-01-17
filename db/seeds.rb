2a3117a12743102f52ac07c5584bf065e6daadf0
Student <example@example.com>
2020-01-17 14:25:10 -0800

    fix views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
 
-50.times do |index|
-  Product.create!(name: Faker::Games::Pokemon.name, cost: Faker::Games::Pokemon.name, country: Faker::Games::Pokemon.name)
+50.times do
+  name = Faker::Games::Pokemon.name
+  description = Faker::Lorem.paragraph(12)
+  product = Product.create!(name: name, cost: 1, country: name)
+  5.times do
+    name = Faker::Games::Pokemon.name
+    review = Review.new(name: name, body: 1289038901238129803189023890129038901238901293801298039801238901283901298038901238992388901289013980328902389032890, rating: 1)
+    review.product = product
+    review.save!
+  end
 end
 
-p "created #{Product.count} Pokemon"
+  p "created #{Product.count} Pokemon"
+  p "created #{Review.count} Pokemon"
