084babbdd49f4d6bfe5f662e59d7602bb21e6195
Student <example@example.com>
2020-01-17 11:36:20 -0800

    add seed/faker functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Games::Pokemon.name, cost: Faker::Games::Pokemon.name, country: Faker::Games::Pokemon.name)
+end
+
+p "created #{Product.count} Pokemon"
